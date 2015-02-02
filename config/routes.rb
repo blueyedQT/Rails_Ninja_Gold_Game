05c03065cad38d7d51448db04150e02ef7b86ffb
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-02 15:21:38 -0800

    All sections working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'rpg#index'
   post 'rpg/farm'
+  post 'rpg/cave'
+  post 'rpg/house'
+  post 'rpg/casino'
+  post 'rpg/reset'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
