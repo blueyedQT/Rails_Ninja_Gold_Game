8f6fa1d56f98ee3a83bb96245ec4e8b078c6616a
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-02 14:26:20 -0800

    Working farm

--- app/controllers/rpg_controller.rb
+++ app/controllers/rpg_controller.rb
@@ -1,6 +1,21 @@
 class RpgController < ApplicationController
-	@@gold = 0
+	@@time = Time.new
 	def index
-		@gold = @@gold
+		# session[:messages] = nil
+		# session[:gold] = nil
+		if session[:messages] == nil
+			session[:messages] = Array.new
+			session[:gold] = 0
+		end
+		@gold = session[:gold]
+		@messages = session[:messages].reverse
+	end
+	def farm
+		@rand = rand(10..20)
+		@timestamp = @@time.strftime('%Y/%m/%d %H:%M %p')
+		@message = "Earned #{@rand} gold from the farm! " + @timestamp
+		session[:gold] += @rand
+		session[:messages].push(@message)
+		redirect_to '/'
 	end
 end
